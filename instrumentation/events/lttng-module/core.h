855ce0b020c4c5faf328a910d95663af4ae4974c
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2015-10-05 21:21:27 -0400

    Add core timings (irqsoff, preemptoff) probe
    How to setup the irqsoff tracer for this use-case (needed for use with
    Ftrace, Perf, or LTTng): if the tracing_thresh value is set, all
    durations beyond the specified threshold will emit a tracepoint. If
    unset, then the max duration will be tracked. The current max duration
    can be reset using the tracing_max_latency file.
    
    echo 0 > /sys/kernel/debug/tracing/options/function-trace  # don't need function tracing
    echo 0 > /sys/kernel/debug/tracing/options/ftrace-buffer
    echo preemptirqsoff > /sys/kernel/debug/tracing/current_tracer
    echo 2 > /sys/kernel/debug/tracing/tracing_thresh      # set threshold (e.g. 2 µs)
    echo 1 > /sys/kernel/debug/tracing/tracing_on
    
    The following two commands can also be useful to tweak the
    irqsoff tracer:
    echo 0 > /sys/kernel/debug/tracing/tracing_max_latency # reset max latency
    echo 0 > /sys/kernel/debug/tracing/tracing_thresh      # disable threshold
    
    An example usage of LTTng hooking on this tracepoint:
    
    (as root)
    lttng-sessiond -d
    
    (then, as root or tracing group)
    lttng create --live
    lttng enable-event -k 'core_*'
    lttng start
    lttng view
    [17:34:17.090669588] (+1.569689755) compudjdev core_critical_timing_hit: { cpu_id = 3 }, { ip = 0xFFFFFFFF81102135, parent_ip = 0xFFFFFFFFA0798E13, delta_ns = 252994, irqs_disabled = 0, preempt_disabled = 1, in_softirq = 0, in_irq = 0, in_nmi = 0 }
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

---
+++ instrumentation/events/lttng-module/core.h
@@ -0,0 +1,89 @@
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM core
+
+#if !defined(LTTNG_TRACE_CORE_H) || defined(TRACE_HEADER_MULTI_READ)
+#define LTTNG_TRACE_CORE_H
+
+#include <linux/irqflags.h>
+#include <linux/preempt.h>
+#include "../../../probes/lttng-tracepoint-event.h"
+
+/*
+ * Tracepoint for critical timings max/threshold hit.
+ */
+LTTNG_TRACEPOINT_EVENT(core_critical_timing_hit,
+
+	TP_PROTO(unsigned long ip, unsigned long parent_ip,
+			unsigned long flags, int preempt_cnt,
+			cycles_t delta_ns),
+
+	TP_ARGS(ip, parent_ip, flags, preempt_cnt, delta_ns),
+
+	TP_FIELDS(
+		ctf_integer_hex(unsigned long, ip, ip)
+		ctf_integer_hex(unsigned long, parent_ip, parent_ip)
+		ctf_integer(cycles_t, delta_ns, delta_ns)
+		ctf_integer(uint8_t, irqs_disabled,
+			!!(raw_irqs_disabled_flags(flags)))
+		ctf_integer(uint8_t, preempt_disabled,
+			!!(preempt_cnt & PREEMPT_MASK))
+		ctf_integer(uint8_t, in_softirq,
+			!!(preempt_cnt & SOFTIRQ_MASK))
+		ctf_integer(uint8_t, in_irq,
+			!!(preempt_cnt & HARDIRQ_MASK))
+		ctf_integer(uint8_t, in_nmi,
+			!!(preempt_cnt & NMI_MASK))
+	)
+)
+
+/*
+ * Tracepoint for critical timings start/stop.
+ */
+LTTNG_TRACEPOINT_EVENT_CLASS(core_critical_timing,
+
+	TP_PROTO(unsigned long ip, unsigned long parent_ip,
+			unsigned long flags, int preempt_cnt),
+
+	TP_ARGS(ip, parent_ip, flags, preempt_cnt),
+
+	TP_FIELDS(
+		ctf_integer_hex(unsigned long, ip, ip)
+		ctf_integer_hex(unsigned long, parent_ip, parent_ip)
+		ctf_integer(uint8_t, irqs_disabled,
+			!!(raw_irqs_disabled_flags(flags)))
+		ctf_integer(uint8_t, preempt_disabled,
+			!!(preempt_cnt & PREEMPT_MASK))
+		ctf_integer(uint8_t, in_softirq,
+			!!(preempt_cnt & SOFTIRQ_MASK))
+		ctf_integer(uint8_t, in_irq,
+			!!(preempt_cnt & HARDIRQ_MASK))
+		ctf_integer(uint8_t, in_nmi,
+			!!(preempt_cnt & NMI_MASK))
+	)
+)
+
+LTTNG_TRACEPOINT_EVENT_INSTANCE(core_critical_timing,
+
+	core_critical_timing_start,
+
+	TP_PROTO(unsigned long ip, unsigned long parent_ip,
+			unsigned long flags, int preempt_cnt),
+
+	TP_ARGS(ip, parent_ip, flags, preempt_cnt)
+)
+
+
+LTTNG_TRACEPOINT_EVENT_INSTANCE(core_critical_timing,
+
+	core_critical_timing_stop,
+
+	TP_PROTO(unsigned long ip, unsigned long parent_ip,
+			unsigned long flags, int preempt_cnt),
+
+	TP_ARGS(ip, parent_ip, flags, preempt_cnt)
+)
+
+#endif /* LTTNG_TRACE_CORE_H */
+
+/* This part must be outside protection */
+#include "../../../probes/define_trace.h"
